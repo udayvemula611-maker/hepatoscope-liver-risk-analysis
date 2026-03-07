60319c77e9d716cbac10dce11140321260fb0ada
Uday Vemula <udayvemula611@gmail.com>
2026-03-07 22:06:12 +0530

    fixed build issues

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -34,24 +34,24 @@ const buttonVariants = cva(
         // Link button style: resembles a text link but acts as a button.
         link: "text-primary underline-offset-4 hover:underline",
       },
-    },
-    size: {
-      // Default button size.
-      default: "h-9 px-4 py-2 has-[>svg]:px-3",
-      // Extra-small button size.
-      xs: "h-6 gap-1 rounded-md px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*=\'size-\\'])]:size-3",
-      // Small button size.
-      sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-      // Large button size.
-      lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-      // Icon-only button size (default).
-      icon: "size-9",
-      // Extra-small icon-only button size.
-      "icon-xs": "size-6 rounded-md [&_svg:not([class*=\'size-\\'])]:size-3",
-      // Small icon-only button size.
-      "icon-sm": "size-8",
-      // Large icon-only button size.
-      "icon-lg": "size-10",
+      size: {
+        // Default button size.
+        default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        // Extra-small button size.
+        xs: "h-6 gap-1 rounded-md px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*=\'size-\\'])]:size-3",
+        // Small button size.
+        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+        // Large button size.
+        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        // Icon-only button size (default).
+        icon: "size-9",
+        // Extra-small icon-only button size.
+        "icon-xs": "size-6 rounded-md [&_svg:not([class*=\'size-\\'])]:size-3",
+        // Small icon-only button size.
+        "icon-sm": "size-8",
+        // Large icon-only button size.
+        "icon-lg": "size-10",
+      },
     },
     defaultVariants: {
       variant: "default", // Sets the default visual variant.
